6a2e60b8dcc4c5184e3fe87a794e7ad12f3a6ece
DomiAndi <lnj20120798@gmail.com>
2024-10-25 12:26:11 -0600

    Se agregaron cambios a tres archivos JS

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,22 @@
+// Inicializar una variable con un valor
+var producto = "Monitor de 20 pulgadas"
+console.log(producto)
+
+// Las variables se pueden reasifnar
+producto = 'Monitor de 19 pulgadas';
+console.log(producto);
+
+// JS es un lenguaje dinamico
+producto = 20;
+console.log(producto);
+
+// Se pueden inicializar y asignar valores despues 
+var disponible;
+disponible = true;
+
+disponible = false;
+
+// Inicializar multiples variables
+var categoria = 'Computadoras';
+    marca = "Marca famosa"
+    calificacion = 5;
