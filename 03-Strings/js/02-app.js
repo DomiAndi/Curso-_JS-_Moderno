3e5cd72a5fb93eaa691bcaf83f0929f4f4e04d3d
DomiAndi <lnj20120798@gmail.com>
2024-10-28 17:54:42 -0600

    Se agrega contenido a las practicas

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,13 @@
+const producto = 'Monitor de 20 pulgadas';
+
+console.log(producto);
+
+// Conocer cantidad de letras de la cadena de texto
+console.log(producto.length);
+
+
+// console.log(producto.indexOf('Tablet'));
+
+
+console.log(producto.includes('Tablet'));
+console.log(producto.includes('pulgadas'));
---
+++ 03-Strings/js/03-app.js
@@ -0,0 +1,12 @@
+const producto = 'Monitor de 20 pulgadas';
+const precio = '30 USD';
+
+// console.log(producto.concat(precio));
+// console.log(producto.concat('En descuento'));
+
+console.log(producto + " Con precio de: " + producto);
+
+// console.log("El producto: " + producto + " tiene prtecio de " + precio);
+console.log("El producto: ", producto, " tiene prtecio de ", precio);
+
+console.log(`El producto ${producto} tiene un precio de $ ${precio}`);
