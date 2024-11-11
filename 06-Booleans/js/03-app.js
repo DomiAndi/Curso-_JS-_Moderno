bf3ed5baf46d345835fa95c1fc66e395f6803d65
DomiAndi <lnj20120798@gmail.com>
2024-11-10 22:56:26 -0600

    Actividades del 01 a 09 de la carpeta objetos realizados

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,11 @@
+const autentificado = true;
+
+// if(autentificado){
+    console.log('Si puedes ver Amazon Prime')
+//    }  else {
+        console.log('No, no puedes verlo')
+// }
+
+// Operador ternario
+
+console.log( autentificado ? 'Si esta autentificado' : 'No esta autentificado');
